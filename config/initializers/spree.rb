89bd038d30730df9889eb954b37531b0bac2b64d
Piotr Leniec <piotr.leniec@sparksolutions.co>
2018-12-07 13:28:41 +0100

    configure host & protocol

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -18,3 +18,6 @@ end
 Spree.user_class = "Spree::User"
 
 Rails.application.config.spree.payment_methods << Spree::PaymentMethod::AfterpayProvider
+
+Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+Rails.application.routes.default_url_options[:protocol] = 'http'
